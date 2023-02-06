8eebece5514f39e9dff65828f72d3780cb5740d1
santhoshcameo <santhosh.cameo@gmail.com>
2023-02-06 16:13:07 +0100

    unified cohorts api dev

--- services/base/kaapana-backend/docker/files/app/storage/routers.py
+++ services/base/kaapana-backend/docker/files/app/storage/routers.py
@@ -5,8 +5,75 @@ from app.config import settings
 import json
 import os
 import socket
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from sqlalchemy.orm import Session
+from app.dependencies import get_db
 
 router = APIRouter(tags=["storage"])
+
+def requests_retry_session(
+    retries=16,
+    backoff_factor=1,
+    status_forcelist=[404, 429, 500, 502, 503, 504],
+    session=None,
+    use_proxies=False
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    if use_proxies is True:
+        proxies = {
+            'http': os.getenv('PROXY', None),
+            'https': os.getenv('PROXY', None),
+            'no_proxy': 'airflow-service.flow,airflow-service.flow.svc,' \
+                'ctp-dicom-service.flow,ctp-dicom-service.flow.svc,'\
+                    'dcm4chee-service.store,dcm4chee-service.store.svc,'\
+                        'opensearch-service.meta,opensearch-service.meta.svc'\
+                            'kaapana-backend-service.base,kaapana-backend-service.base.svc,' \
+                                'minio-service.store,minio-service.store.svc'
+        }
+        session.proxies.update(proxies)
+    
+    return session
+
+
+@router.get('/datasets')
+def listbuckets(db: Session = Depends(get_db)):
+    """Return List of Minio buckets
+    """
+    '''buckets = minio.list_buckets()
+    data = []
+    for bucket in buckets:
+        data.append({
+          'name':str(bucket.name),
+          'creation_date': bucket.creation_date,
+          'webui':f'https://{settings.hostname}/minio/{bucket.name}'
+        })
+    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+    #print(requests.get_cohort_names)'''
+
+
+    #return data
+
+    #data =  requests.get("https://vm-129-196.cloud.dkfz-heidelberg.de/kaapana-backend/client/cohort-names")
+    with requests.Session() as s:
+      r = requests_retry_session(session=s).get('http://kaapana-backend-service.base.svc:5000/client/cohort-names')
+
+    #r = requests.get(f'http://kaapana-backend-service.base.svc:5000/client/cohort-names',verify=False)
+    print(r.json)
+    print(r)
+    return r.json()
 
 @router.get('/buckets')
 def listbuckets(minio=Depends(get_minio)):
