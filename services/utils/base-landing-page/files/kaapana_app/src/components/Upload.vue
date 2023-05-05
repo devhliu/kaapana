2ce0ccbfd5ccf0367ee908dc46c1dbac76d13b5b
jens <jens.beyermann@dkfz-heidelberg.de>
2023-05-05 12:14:59 +0200

    added more-info button

--- services/utils/base-landing-page/files/kaapana_app/src/components/Upload.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/Upload.vue
@@ -1,100 +1,142 @@
+<template>
+  <div class="dropzone">
+    <v-container grid-list-lg text-left >
+      <h1>Data upload</h1> 
+      <v-row dense>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title class="text-h5">
+              Option 1 (preferred): Using the DICOM receiver port.
+            </v-card-title>
+            <v-card-text>
+              If you have images locally you can use e.g. DCMTK. However, any tool that sends images to a DICOM receiver can be used. Here is an example of sending images with DCMTK:
+              <br>
+              <br>
+              <code>
+                dcmsend -v ip-address of server 11112  --scan-directories --call aetitle-of-images-used-for-filtering --scan-pattern '*'  --recurse data-dir-of-DICOM-images
+              </code>
+            </v-card-text>
 
-<template lang="pug">
-    file-pond(
-      allow-multiple="true"
-      :label-idle='labelIdle'
-      label-tap-to-undo="Remove from list"
-      :accepted-file-types='acceptedFileTypes'
-    )
+           
+
+
+
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title class="text-h5">
+              Option 2: Store data to file storage
+            </v-card-title>
+            <v-card-text>
+              <v-icon large>mdi-numeric-1-circle</v-icon>&nbsp; Store DICOMS, niftis or any data you want to use in a workflow as a zip file via the dropzone
+              <upload labelIdle="Dicoms, ITK images or any other data"></upload>
+              <br> Uploaded nifti data can also be converterted to DICOM and send to our internal PACS via the "convert-nifitis-to-dicoms-and-import-to-internal-pacs". Zipped DICOMS can be send to the PACS with the "import-dicoms-in-zip-to-internal-pacs" workflow.<br><br>
+              <v-icon large>mdi-numeric-2-circle</v-icon>&nbsp;
+              <v-btn
+                color="primary"
+                @click="() => (this.workflowDialog = true)"
+              > 
+                Import the data to the platform 
+                <v-icon>mdi-play-box</v-icon>
+              </v-btn>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn @click="showSubfield2 = !showSubfield2" text target="_blank">More info</v-btn>
+            </v-card-actions>
+            
+            <v-expand-transition>
+              <v-card-text v-if="!showSubfield2">
+                In order to convert your uploaded nifti (.nii.gz) or nrrd (.nrrd) files into the DICOM format and send them to the PACS, 
+                    please provide the data in the <a href="https://github.com/MIC-DKFZ/nnUNet/blob/master/documentation/dataset_format_inference.md">nnunet dataset format</a>. In short it should have the following structure:<br><br>
+
+<pre>
+Dataset001_BrainTumour
+    ├── dataset.json
+    ├── imagesTr
+    |   ├── BRATS_001_0000.nii.gz
+    │   ├── BRATS_001_0001.nii.gz
+    │   ├── BRATS_001_0002.nii.gz
+    │   ├── BRATS_001_0003.nii.gz
+    │   ├── BRATS_002_0000.nii.gz
+    │   ├── BRATS_002_0001.nii.gz
+    │   ├── BRATS_002_0002.nii.gz
+    │   ├── BRATS_002_0003.nii.gz
+    │   ├── ...
+    ├── imagesTs  # optional
+    │   ├── BRATS_485_0000.nii.gz
+    │   ├── BRATS_485_0001.nii.gz
+    │   ├── BRATS_485_0002.nii.gz
+    │   ├── BRATS_485_0003.nii.gz
+    │   ├── BRATS_486_0000.nii.gz
+    │   ├── BRATS_486_0001.nii.gz
+    │   ├── BRATS_486_0002.nii.gz
+    │   ├── BRATS_486_0003.nii.gz
+    │   ├── ...
+    └── labelsTr
+        ├── BRATS_001.nii.gz
+        ├── BRATS_002.nii.gz
+        ├── ...
+</pre><br>
+                Where imageTr means training data, imageTS means test data and labelsTr contains the segmentation labels. 
+              </v-card-text>
+            </v-expand-transition>
+
+
+
+          </v-card>
+        </v-col>
+
+        <v-dialog v-model="workflowDialog" width="500">
+        <WorkflowExecution
+          :onlyClient=true
+          kind_of_dags="minio"
+          @successful="() => (this.workflowDialog = false)"
+        />
+        </v-dialog>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
-<script>
-//      accepted-file-types="zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed"
-import vueFilePond, { setOptions } from 'vue-filepond';
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'; // /dist/filepond-plugin-file-validate-type.esm.js';
-import 'filepond/dist/filepond.min.css';
+<script lang="ts">
 
-const FilePond = vueFilePond( FilePondPluginFileValidateType );
+import Vue from 'vue';
+import { mapGetters } from "vuex";
+import Upload from "@/components/Upload.vue";
+import WorkflowExecution from "@/components/WorkflowExecution.vue";
 
-export default {
-  name: 'upload',
-  props: {
-    labelIdle: String,
-    acceptedFileTypes: String,
-  },
+export default Vue.extend({
   components: {
-    FilePond
+    Upload,
+    WorkflowExecution
   },
+  data: () => ({
+    workflowDialog: false,
+    supported: true,
+    showSubfield2: true,
+  }),
   mounted() {
-    setOptions({
-      chunkUploads: true,
-      chunkForce: true,
-      chunkSize: 1024 * 1024 * 1, // 50MB
-      beforeAddFile: (file) => {
-        let filepath = ""
-        if (file.relativePath == ''){
-          filepath = file.filename
-        } else {
-          filepath = file.relativePath
-        }
-        file.setMetadata("filepath", filepath)
-      },
-      server: {
-        // From https://pqina.nl/filepond/docs/api/server/#process-1
-        url: '/kaapana-backend/client/minio-file-upload',
-        // process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-        //   // fieldName is the name of the input field
-        //   // file is the actual file object to send
-        //   const formData = new FormData();
-
-        //   //////////////////////////////
-        //   // Adjusted by Kaapana...
-        //   let filename = ""
-        //   if (file._relativePath == ''){
-        //     filename = file.name
-        //   } else {
-        //     filename = file._relativePath
-        //   }
-        //   formData.append(fieldName, file, filename);
-        //   //////////////////////////////
-
-        //   const request = new XMLHttpRequest();
-        //   request.open('POST', '/kaapana-backend/client/post-minio-object-to-uploads');
-
-        //   // Should call the progress method to update the progress to 100% before calling load
-        //   // Setting computable to false switches the loading indicator to infinite mode
-        //   request.upload.onprogress = (e) => {
-        //     progress(e.lengthComputable, e.loaded, e.total);
-        //   };
-
-        //   // Should call the load method when done and pass the returned server file id
-        //   // this server file id is then used later on when reverting or restoring a file
-        //   // so your server knows which file to return without exposing that info to the client
-        //   request.onload = function () {
-        //     if (request.status >= 200 && request.status < 300) {
-        //       // the load method accepts either a string (id) or an object
-        //       load(request.responseText);
-        //     } else {
-        //       // Can call the error method if something is wrong, should exit after
-        //       error('oh no');
-        //     }
-        //   };
-
-        //   request.send(formData);
-
-        //   // Should expose an abort method so the request can be cancelled
-        //   return {
-        //     abort: () => {
-        //       // This function is entered if the user has tapped the cancel button
-        //       request.abort();
-
-        //       // Let FilePond know the request has been cancelled
-        //       abort();
-        //     },
-        //   };
-        // },
-      },
-    });
+    const { userAgent } = navigator
+    if (userAgent.includes('Firefox/')) {
+      this.supported = false
+    } else {
+      this.supported = true
+    }
+  },
+  computed: {
+    ...mapGetters(['currentUser', 'isAuthenticated'])
+  },
+  methods: {
   }
-}
+})
 </script>
+
+<style lang="scss">
+.upload {
+  margin-top: 10px; 
+  padding-top: 100px;
+  padding-bottom: 10px;
+}
+</style>
